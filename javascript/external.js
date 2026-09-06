837d82bd894f6d6b63ef1200311b8592d079c272
Opeyemi Olatunde <61982300+olatundeopeyemi0001@users.noreply.github.com>
2026-09-06 11:16:02 -0500

    n

--- javascript/external.js
+++ javascript/external.js
@@ -1,4 +1,4 @@
 // A prompt box is used to prompt users to input a value before entering a page.
-const year = new Date().getFullYear();
+
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write("<h1>Hello, welcome " + user_name + "!</h1>");
