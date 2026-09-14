800594bc1b38529b2991bf17e5eee96fed4e52c2
Opeyemi Olatunde <61982300+olatundeopeyemi0001@users.noreply.github.com>
2026-09-13 23:54:15 -0500

    broken line

--- javascript/external.js
+++ javascript/external.js
@@ -4,7 +4,7 @@
 //document.write("<h1>Hello, welcome " + user_name + "!</h1>");
 var webmaps =
 [
-  ["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit developed by Enterprise Technology Solutions of The Texas  General Land Office is a decision-support resources. This is where I would write my second sentence about the oil Spill Toolkit. This is where I would write my third sentence a bout the Oil spill Toolkit. This is where I would write my fourth sentence about the Oil Spill Toolkit."],
+  ["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit developed by Enterprise Technology Solutions of The Texas  General Land Office is a decision-support resources.\n This is where I would write my second sentence about the oil Spill Toolkit.\n This is where I would write my third sentence a bout the Oil spill Toolkit.\n This is where I would write my fourth sentence about the Oil Spill Toolkit."],
   ["Texas Ecosystems Analytical Mapper", "http://tpwd.texas.gov/gis/team/", "The Texas Parks and Wildlife's Landscape Ecology program is great."]
 ];
 
