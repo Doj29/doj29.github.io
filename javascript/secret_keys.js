ea227c4b6cfeb0269ba5c67358bb09541c95fc01
Opeyemi Olatunde <61982300+olatundeopeyemi0001@users.noreply.github.com>
2026-09-21 17:59:44 -0500

    m

--- javascript/secret_keys.js
+++ javascript/secret_keys.js
@@ -1,1 +1,1 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiZG9qMjkiLCJhIjoiY211YnFseTg1MDFoMTMwb284c293dTZvNCJ9.KDC2iqqXFyag4OIvnXSnFA";
+mapboxgl_access_token = "pk.eyJ1IjoiZG9qMjkiLCJhIjoiY211YnFseTg1MDFoMTMwb284c293dTZvNCJ9.KDC2iqqXFyag4OIvnXSnFA";
